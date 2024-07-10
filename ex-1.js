2cb0c9379167a7ccc0b9c767a2cadeeca5d12d90
Joom <jnwongsa@gmail.com>
2024-07-10 14:55:04 +0700

    update:ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,13 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i = 0, j = numbers.length - 1; i < j; ) {
+    let sum = numbers[i] + numbers[j];
+    if (sum === target) {
+      return [i + 1, j + 1];
+    } else if (sum < target) {
+      i++;
+    } else {
+      j--;
+    }
+  }
+}
